60bf57e792dbe06ae27fcf92bb66c1a0eecabbbc
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-10-27 14:22:40 +0200

    add css to login and home components

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
         <div className="home-pg">
             <button className="btn add">Add new deal card</button>
-            <button className="btn search">Search a card</button>
-            <input type="text" placeholder="search..."></input>
+            <h3> OR </h3>
+            <div className="search-area">
+                <button className="btn search">Search a card</button>
+                <input className="search-input" type="text" placeholder="search..."></input>
+            </div>
 
         </div>
     );
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
     return (
         <div className="login-pg">
             <h1>Deal Card</h1>
-            <input type="text" placeholder="Enter username"></input>
+            <input className="login-input" type="text" placeholder="Enter username"></input>
             <Link to="/home"> <button className="btn login">Login</button></Link>
         </div>
     );
