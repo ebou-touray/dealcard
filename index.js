04bf0a271772618526b49e902b688af2fffc8723
bslzz <bishalkoid@gmail.com>
2020-10-28 20:01:30 +0200

    imported mongoose connection from db folder and imported .env file

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+//import env file
+require('dotenv').config();
+
 //init app
 const app = express();
+
+//import mongodb from db folder
+require('./db/initMongoose');
 
 const PORT = process.env.PORT || 5000;
 
