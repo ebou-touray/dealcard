c029e0b2f2ac82862c1689626ecfa9e7dec9751d
bslzz <bishalkoid@gmail.com>
2020-11-17 11:04:37 +0200

    find and modify added to mongoose

--- backend/db/initMongoose.js
+++ backend/db/initMongoose.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 module.exports = mongoose.connect(process.env.MONGODB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 //create connection
