8e2bb0a7b5463a6f13113b7b6ecfec164b7a3112
bslzz <bishalkoid@gmail.com>
2020-11-17 10:57:04 +0200

    removed searchbar from navbar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -3,11 +3,6 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
-  const [searchInput, setSearchInput] = useState('');
-  const searchHandler = (e) => {
-    setSearchInput(e.target.value);
-  };
-
   return (
     <div className="navBar">
       <Link to="/">
@@ -16,16 +11,6 @@ const NavBar = () => {
       <Link to="/form">
         <button className="btn add">Add new deal card</button>
       </Link>
-      <div className="search-area">
-        <button className="btn search">Search a card</button>
-        <input
-          className="search-input"
-          type="text"
-          placeholder="search..."
-          onChange={searchHandler}
-          value={searchInput}
-        ></input>
-      </div>
     </div>
   );
 };
