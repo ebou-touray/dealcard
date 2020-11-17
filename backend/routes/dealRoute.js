2da74a467e040843b932ec26c82ed6cec4f1ea66
bslzz <bishalkoid@gmail.com>
2020-11-17 11:04:16 +0200

    created edit route from controller

--- backend/routes/dealRoute.js
+++ backend/routes/dealRoute.js
@@ -7,5 +7,6 @@ router.route('/postDeal').post(dealController.addDeal);
 router.route('/getAllDeals').get(dealController.getAllDeals);
 router.route('/deals/:id').delete(dealController.deleteDeals);
 router.route('/:id').get(dealController.findDealsById);
+router.route('/edit/:id').put(dealController.editDeals);
 
 module.exports = router;
